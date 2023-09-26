d6e0118ba8ad0c39fa0e5fac4ddc98bcccb509be
rulanugrh <nextarituenak01@gmail.com>
2023-09-26 11:12:55 +0700

    fix: name of function

--- Amalthea/basic.py
+++ Amalthea/basic.py
@@ -19,35 +19,35 @@ class BasicMath:
 class LuasBangunDatar:
     
     @staticmethod
-    def persegi(s: float) -> float:
+    def luasPersegi(s: float) -> float:
         return s * s
         
     @staticmethod
-    def segitiga(a: float, t: float) -> float:
+    def luasSegitiga(a: float, t: float) -> float:
         return 0.5 * a * t
         
     @staticmethod
-    def persegiPanjang(p: float, l: float) -> float:
+    def luasPersegiPanjang(p: float, l: float) -> float:
         return p * l
         
     @staticmethod
-    def trapesium(a: float, b: float, t: float) -> float:
+    def luasTrapesium(a: float, b: float, t: float) -> float:
         return 0.5 * (a + b) * t
     
     @staticmethod
-    def layangLayang(d1: float, d2: float) -> float:
+    def luasLayangLayang(d1: float, d2: float) -> float:
         return (d1 * d2) / 2
     
     @staticmethod
-    def belahKetupat(d1: float, d2: float) -> float:
+    def luasBelahKetupat(d1: float, d2: float) -> float:
         return (d1 * d2) / 2
     
     @staticmethod
-    def jajarGenjang(a: float, t: float) -> float:
+    def luasJajarGenjang(a: float, t: float) -> float:
         return a * t
     
     @staticmethod
-    def lingkaran(r: int) -> float:
+    def luasLingkaran(r: int) -> float:
         if r%7 == 0:
             return 22/7 * r *r
         else:
@@ -55,39 +55,39 @@ class LuasBangunDatar:
 
 class KelilingBangunDatar:
     @staticmethod
-    def persegi(s: float) -> float:
+    def kelilingPersegi(s: float) -> float:
         return 4 * s
     
     @staticmethod
-    def segitigaSamaSisi(s: float) -> float:
+    def kelilingSegitigaSamaSisi(s: float) -> float:
         return 3 * s
     
     @staticmethod
-    def segitigaSamaKaki(a: float, s: float) -> float:
+    def kelilingSegitigaSamaKaki(a: float, s: float) -> float:
         return a + s + s
     
     @staticmethod
-    def persegiPanjang(p: float, l: float) -> float:
+    def kelilingPersegiPanjang(p: float, l: float) -> float:
         return (2 * p) + (2 * l)
     
     @staticmethod
-    def layangLayang(ab: float, bc: float) -> float:
+    def kelilingLayangLayang(ab: float, bc: float) -> float:
         return 2 * (ab + bc)
     
     @staticmethod
-    def trapesium(ab: float, bc: float, cd: float, da: float) -> float:
+    def kelilingTrapesium(ab: float, bc: float, cd: float, da: float) -> float:
         return ab + bc + cd + da
     
     @staticmethod
-    def belahKetupat(s: float) -> float:
+    def kelilingBelahKetupat(s: float) -> float:
         return 4 * s
     
     @staticmethod
-    def jajarGenjang(ab: float, bc: float, cd: float, ad: float) -> float:
+    def kelilingJajarGenjang(ab: float, bc: float, cd: float, ad: float) -> float:
         return ab + bc + cd + ad
     
     @staticmethod
-    def lingkaran(d: int) -> float:
+    def kelilingLingkaran(d: int) -> float:
         if d%7 == 0:
             return 22/7 * d
         else:
